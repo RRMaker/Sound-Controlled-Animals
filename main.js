037a7a87b2bb1139b853f1cc0803a87e206e4c07
RRMaker <83099525+RRMaker@users.noreply.github.com>
2022-03-12 16:17:54 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-//https://teachablemachine.withgoogle.com/models/a5dNiqovY/
-function startClassification(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/a5dNiqovY/model.json",modelLoaded);
-}
-function modelLoaded(){
-    classifier.classify(gotResults);
-    console.log("model is ready!");
-}
